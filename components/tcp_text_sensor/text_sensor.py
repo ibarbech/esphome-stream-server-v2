714ff776a165889bed805f29ac16c820b60cd8fa
Iván Barbecho Delgado <delgado.extern@univention.de>
2023-01-18 16:24:35 +0100

    update

--- components/tcp_text_sensor/text_sensor.py
+++ components/tcp_text_sensor/text_sensor.py
@@ -1,14 +1,14 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import text_sensor
-from esphome.const import CONF_ID
+from esphome.const import CONF_ID, CONF_PORT
 
-empty_text_sensor_ns = cg.esphome_ns.namespace('tcp_text_sensor')
-TCPTextSensor = empty_text_sensor_ns.class_('TCPTextSensor', text_sensor.TextSensor, cg.Component)
+tcp_text_sensor_ns = cg.esphome_ns.namespace('tcp_text_sensor')
+TCPTextSensor = tcp_text_sensor_ns.class_('TCPTextSensor', text_sensor.TextSensor, cg.Component)
 
 CONFIG_SCHEMA = text_sensor.TEXT_SENSOR_SCHEMA.extend({
     cv.GenerateID(): cv.declare_id(TCPTextSensor),
-    cv.Required("port"): cv.port,
+    cv.Required(CONF_PORT): cv.port,
 }).extend(cv.COMPONENT_SCHEMA)
 
 
